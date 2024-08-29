7fec36e48c8ea8b98ee01abd580847705f862b02
Josh Wolfe <josh@okcupid.com>
2024-08-29 16:24:26 -0400

    fix comment parsing between include statements

--- parser_test.go
+++ parser_test.go
@@ -223,6 +223,19 @@ func TestExtractObject(t *testing.T) {
 		assertDeepEqual(t, got, expected)
 	})
 
+	t.Run("parse a comment between two includes", func(t *testing.T) {
+		parser := newParser(strings.NewReader(
+			`include "testdata/a.conf"
+			# comment
+			include "testdata/b.conf"
+		`))
+		parser.advance()
+		expected := Object{"a": Int(1), "b": Int(2)}
+		got, err := parser.extractObject()
+		assertNoError(t, err)
+		assertDeepEqual(t, got, expected)
+	})
+
 	t.Run("parse correctly if the last line is a comment", func(t *testing.T) {
 		config := `{
 			a: 1
